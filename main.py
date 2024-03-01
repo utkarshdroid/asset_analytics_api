7ec36a7aca50fb457e3f51f08dfa954d98eb9c18
utkarsh-sharma4 <utkarsh.sharma4@byjus.com>
2024-03-01 13:20:50 +0530

    added logic in middleware

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from src.models.firms import Firm
 from src.models.commitment import Commitment
-from sqlalchemy import select, and_
+
 import src.utils.asset_business_logic as asset_utils
-from src.utils.db_utils import database, connect_to_db, disconnect_from_db
-from src.models.db_models_metadata import firms, commitments
+from src.utils.db_utils  import connect_to_db, disconnect_from_db
+
 
 app = FastAPI()
 
@@ -30,36 +30,11 @@ async def shutdown_event():
     await disconnect_from_db()
 
 
-f = open("./db/data.json")
-data = json.load(f)
-
-INVESTOR_IDS: List[int] = []
-FIRMS: List[Firm] = []
-for firm in data["firms"]:
-    INVESTOR_IDS.append(firm["firm_id"])
-    parsed_firm = Firm(**firm)
-    FIRMS.append(parsed_firm)
-
-
 @app.get("/api/investors", response_model=list[Firm])
 async def get_investors():
-    query = firms.select()
-    result = await database.fetch_all(query)
-    
-    # Convert the result set to Pydantic models
-    investors = [Firm(**row) for row in result]
-    return investors
+  return asset_utils.get_investor_commitments_details
 
 @app.get("/api/investor/commitment/{asset_class}/{investor_id}")
 async def get_commitments(investor_id: int, asset_class: str) -> List[Commitment]:
-    print(investor_id,asset_class)
-    query = select([commitments]).where(
-        and_(
-            commitments.c.asset_class == asset_class,
-            commitments.c.firm_id == investor_id
-        )
-    )
-    results = await database.fetch_all(query)
-    commitments_list = [Commitment(**row) for row in results]
-    return commitments_list 
+    return asset_utils.get_investor_commitments_details
 
--- src/utils/asset_business_logic.py
+++ src/utils/asset_business_logic.py
@@ -4,31 +4,25 @@ from src.models.commitment import Commitment
 import json
 from src.utils.constants import ASSET_CLASSES
 import src.utils.db_utils as db_utils
+from sqlalchemy import select, and_
+from src.models.db_models_metadata import firms, commitments
+from src.utils.db_utils import database, connect_to_db, disconnect_from_db
+
+async def get_all_investor_funds()->List[Firm]:
+    query = firms.select()
+    result = await database.fetch_all(query)
+    investors = [Firm(**row) for row in result]
+    return investors
 
 
-def get_all_investor_funds()->List[Firm]:
-    firms = db_utils.get_all_firms()
-    FIRMS: List[Firm] = []
-    for firm in firms:
-        parsed_firm = Firm(**firm)
-        FIRMS.append(parsed_firm)
-    return FIRMS
 
-
-
-def get_investor_commitments_details()-> List[Commitment]:
-    firms = db_utils.get_all_firms()
-    INVESTOR_IDS: List[int]= []
-    for firm in firms:
-        INVESTOR_IDS.append(firm["firm_id"])
-    if request.investor_id not in INVESTOR_IDS:
-        raise Exception(status_code=404, detail=f"investor with id {request.investor_id} not found")
-    if request.asset_class not in ASSET_CLASSES:
-        raise Exception(status_code=404, detail=f"asset class {request.asset_class} does not exist")
-    result: List[Commitment] = []
-    commitments = db_utils.select_commitments_by_firm_id(request.insestor_id, request.asset_class)
-    for commitment in commitments :
-        # if commitment["firm_id"] == request.investor_id and commitment["asset_class"] == request.asset_class:
-            parsed_commitment = Commitment(**commitment)
-            result.append(parsed_commitment)
-    return result
+async def get_investor_commitments_details(investor_id:int, asset_class:str)-> List[Commitment]:
+    query = select([commitments]).where(
+        and_(
+            commitments.c.asset_class == asset_class,
+            commitments.c.firm_id == investor_id
+        )
+    )
+    results = await database.fetch_all(query)
+    commitments_list = [Commitment(**row) for row in results]
+    return commitments_list 
